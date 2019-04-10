2205b5fc91129ccf15942b83604cd1cb30eb213e
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-10 16:58:42 -0300

    Fix morgan

--- src/server.js
+++ src/server.js
@@ -32,7 +32,9 @@ app.use((req, res, next) => {
   return next();
 });
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
